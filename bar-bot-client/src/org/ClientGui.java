8a210ccd7102535c6ed84464b14a99a06a2efc51
seth478 <seth478@yahoo.com>
2014-02-28 01:47:33 -0500

    Completed Graphical Compenents of GUI. Functionality coming soon

--- bar-bot-client/src/org/ClientGui.java
+++ bar-bot-client/src/org/ClientGui.java
@@ -16,7 +16,7 @@ public class ClientGui extends JFrame implements ActionListener{
 	/*
 	 * Menu items
 	 */
-	JMenuItem addDrink;
+	JMenuItem createDrink;
 	JMenuItem selectDrink;
 	
 	/*
@@ -25,6 +25,12 @@ public class ClientGui extends JFrame implements ActionListener{
 	 */
 	JPanel selectionPanel;
 	JPanel editPanel;
+	
+	/*
+	 * Selection Panels, Buttons, etc.
+	 */
+	JButton selectionAcceptButton;
+	JButton selectionResetButton;
 	
 	/*
 	 * Edit Panels, Buttons, etc.
@@ -54,7 +60,133 @@ public class ClientGui extends JFrame implements ActionListener{
 		selectionPanel.setLayout( new BorderLayout()); 
 		barbotPanel.add(selectionPanel);
 		
+		/*
+		 * Accept and Cancel Button Panel
+		 * for Drink Selection
+		 */
+		JPanel selectionAcceptPanel = new JPanel();
+		selectionAcceptPanel.setLayout(new BorderLayout());
+		selectionPanel.add(selectionAcceptPanel, BorderLayout.SOUTH);
 		
+		selectionAcceptButton = new JButton("Accept");
+		selectionAcceptPanel.add(selectionAcceptButton, BorderLayout.EAST);
+		
+		selectionResetButton = new JButton("Reset");
+		selectionAcceptPanel.add(selectionResetButton, BorderLayout.WEST);
+		
+		/*
+		 * Panel for Drink Grid
+		 */
+		JPanel gridPanel = new JPanel();
+		gridPanel.setLayout(new GridLayout(4,3,3,3));
+		selectionPanel.add(gridPanel, BorderLayout.CENTER);
+		
+		JButton buttonArray[] = new JButton[12];
+		for (int i = 0; i < 12; i++){
+			buttonArray[i] = new JButton("" + i);
+			gridPanel.add(buttonArray[i]);
+		}
+	
+		
+		/*
+		 * Information Panel that resides on the right side of the screen
+		 * Contains the drink's DRINK NAME, INGREDIENTS, and DESCRIPTION 
+		 */
+		JPanel selectionInformationPanel = new JPanel();
+		selectionInformationPanel.setLayout(new BorderLayout());
+		selectionInformationPanel.setPreferredSize(new Dimension(400, MAXIMIZED_VERT));
+		selectionInformationPanel.setBorder(BorderFactory.createTitledBorder("Drink Information"));
+		selectionPanel.add(selectionInformationPanel, BorderLayout.EAST);
+		
+		
+		JPanel selectNamePanel = new JPanel();
+		selectNamePanel.setLayout(new BorderLayout());
+		selectNamePanel.setBorder( BorderFactory.createEmptyBorder(10, 0, 0, 0));
+		JLabel selectNameLabel = new JLabel("Drink Name:");
+		JTextField selectInfoNameTextField = new JTextField(20);
+		selectInfoNameTextField.setEditable(false);
+		selectNamePanel.add(selectNameLabel, BorderLayout.WEST);
+		selectNamePanel.add(selectInfoNameTextField, BorderLayout.CENTER);
+		selectionInformationPanel.add(selectNamePanel, BorderLayout.NORTH);
+		
+		
+		JPanel selectIngredientPanel = new JPanel();
+		selectIngredientPanel.setLayout(new BoxLayout(selectIngredientPanel, BoxLayout.PAGE_AXIS));
+		selectIngredientPanel.setBorder( BorderFactory.createEmptyBorder(10, 0, 0, 10));
+		selectionInformationPanel.add(selectIngredientPanel, BorderLayout.CENTER);
+		
+		
+		/*
+		 * Header text for the Drink creation field
+		 */
+		JPanel selectIngredientPanelArray[] = new JPanel[13];
+		
+		JLabel selectNameHeaderLabel = new JLabel("Ingredient");
+		selectNameHeaderLabel.setPreferredSize(new Dimension(225,20));
+		JLabel selectSizeHeaderLabel = new JLabel("Size (mL)");
+		JPanel selectSizeHeaderPanel = new JPanel();
+		selectSizeHeaderPanel.add(selectSizeHeaderLabel);
+		
+		selectIngredientPanelArray[12] = new JPanel(new BorderLayout());
+		selectIngredientPanelArray[12].add(selectNameHeaderLabel, BorderLayout.WEST);
+		selectIngredientPanelArray[12].add(selectSizeHeaderPanel, BorderLayout.CENTER);
+		selectIngredientPanel.add(selectIngredientPanelArray[12]);
+		
+		
+		/*
+		 * Handles the Labels, textfields, and remove buttons
+		 * in the Drink creation field
+		 */
+		JPanel selectIngredientNamePanel[] = new JPanel[12];
+		JTextField selectIngredientNameArray[] = new JTextField[12];
+		JPanel selectTextPanelArray[] = new JPanel[12];
+		JTextField selectAmountTextArray[] = new JTextField[12];
+		JPanel selectRemovePanelArray[] = new JPanel[12];
+		//JButton selectRemoveIngredientArray[] = new JButton[12];
+		
+		for (int i = 0; i < 12; i++){
+			
+			selectIngredientPanelArray[i] = new JPanel(new BorderLayout());
+			selectIngredientNamePanel[i] = new JPanel();
+			selectIngredientNameArray[i] = new JTextField(20);
+			selectIngredientNameArray[i].setEditable(false);
+			selectIngredientNamePanel[i].add(selectIngredientNameArray[i]);
+			
+			selectAmountTextArray[i] = new JTextField(3);
+			selectAmountTextArray[i].setEditable(false);
+			selectTextPanelArray[i] = new JPanel();
+			selectTextPanelArray[i].add(selectAmountTextArray[i]);
+			
+			
+			//selectRemoveIngredientArray[i] = new JButton("Remove");
+			//selectRemovePanelArray[i] = new JPanel();
+			//selectRemovePanelArray[i].add(selectRemoveIngredientArray[i]);
+			
+	
+			selectIngredientPanelArray[i].add(selectIngredientNamePanel[i],BorderLayout.WEST);
+			selectIngredientPanelArray[i].add(selectTextPanelArray[i],BorderLayout.CENTER);
+			//selectIngredientPanelArray[i].add(selectRemovePanelArray[i],BorderLayout.EAST);
+			selectIngredientPanel.add(selectIngredientPanelArray[i]);
+		}
+		
+		
+		JPanel selectDescriptionPanel = new JPanel(new BorderLayout());
+		JLabel selectDescriptionLabel = new JLabel("Drink Description:");
+		JTextArea selectInfoDescriptionTextArea = new JTextArea();
+		selectInfoDescriptionTextArea.setEditable(false);
+		selectInfoDescriptionTextArea.setRows(5);
+		selectInfoDescriptionTextArea.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		selectDescriptionPanel.add(selectDescriptionLabel, BorderLayout.NORTH);
+		selectDescriptionPanel.add(selectInfoDescriptionTextArea, BorderLayout.CENTER);
+		selectionInformationPanel.add(selectDescriptionPanel, BorderLayout.SOUTH);
+		
+		
+		
+		//*************************************************************************************
+		/*
+		 * End of Select Panel code. Beginning of Edit Panel code
+		 */
+		//*************************************************************************************
 		
 		
 		/*
@@ -71,38 +203,139 @@ public class ClientGui extends JFrame implements ActionListener{
 		editResetButton = new JButton("Reset");
 		acceptPanel.add(editResetButton, BorderLayout.WEST);
 		
+		
 		/*
-		 * Panel for Drink Grid
+		 * Panel for both Ingredient addition (database and drink)
+		 * and for making drinks
 		 */
-		JPanel gridPanel = new JPanel();
-		gridPanel.setLayout(new GridLayout(4,3));
-		editPanel.add(gridPanel, BorderLayout.CENTER);
-		
-		JButton buttonArray[] = new JButton[12];
-		for (int i = 0; i < 12; i++){
-			buttonArray[i] = new JButton("" + i);
-			gridPanel.add(buttonArray[i]);
-		}
-		
+		JPanel editDrinkPanel = new JPanel();
+		editDrinkPanel.setLayout(new BoxLayout(editDrinkPanel, BoxLayout.LINE_AXIS));
+
+		/*
+		 * Contains all Ingredient addition
+		 */
+		JPanel addIngredientPanel = new JPanel();
+		addIngredientPanel.setLayout(new BoxLayout(addIngredientPanel, BoxLayout.PAGE_AXIS));
+		addIngredientPanel.setPreferredSize(new Dimension(400, MAXIMIZED_VERT));
 		
 		/*
-		 * Information Panel that resides on the right side of the screen
-		 * Contains the currently selected DRINK NAME, INGREDIENTS, and DESCRIPTION 
+		 * Contains items for adding ingredients
+		 * to the drink creator
+		 */
+		JPanel addIngredientDrinkPanel = new JPanel();
+		addIngredientDrinkPanel.setBorder(BorderFactory.createTitledBorder("Add Ingredient to Drink"));
+		
+		JComboBox addIngredientDrinkBox = new JComboBox();
+		addIngredientDrinkBox.addItem(new String("Hello World"));
+		addIngredientDrinkPanel.add(addIngredientDrinkBox);
+		JButton addIngredientDrinkButton = new JButton("Accept");
+		addIngredientDrinkPanel.add(addIngredientDrinkButton);
+		addIngredientPanel.add(addIngredientDrinkPanel);
+		
+		/*
+		 * Contains items for adding ingredients
+		 * to the database
+		 */
+		JPanel addIngredientDataPanel = new JPanel();
+		addIngredientDataPanel.setBorder(BorderFactory.createTitledBorder("Add Ingredient to Database"));
+		
+		JTextField addIngredientDataField = new JTextField(20);
+		addIngredientDataPanel.add(addIngredientDataField);
+		JButton addIngredientDataButton = new JButton("Accept");
+		addIngredientDataPanel.add(addIngredientDataButton);
+		addIngredientPanel.add(addIngredientDataPanel);
+		
+		editDrinkPanel.add(addIngredientPanel);
+		
+		/*
+		 * Information Editing Panel that resides on the right side of the screen
+		 * Contains the drink's DRINK NAME, INGREDIENTS, and DESCRIPTION 
 		 */
 		JPanel informationPanel = new JPanel();
 		informationPanel.setLayout(new BorderLayout());
-		informationPanel.setBackground(Color.WHITE);
-		editPanel.add(informationPanel, BorderLayout.EAST);
+		informationPanel.setPreferredSize(new Dimension(400, MAXIMIZED_VERT));
+		informationPanel.setBorder(BorderFactory.createTitledBorder("Make a Drink"));
+		editDrinkPanel.add(informationPanel);
 		
-		JTextField infoNameTextField = new JTextField("NAME GOES HERE");
-		informationPanel.add(infoNameTextField, BorderLayout.NORTH);
+		editPanel.add(editDrinkPanel, BorderLayout.WEST);
 		
-		JLabel infoIngredientLabel = new JLabel("INGREDIENT LIST");
-		informationPanel.add(infoIngredientLabel, BorderLayout.CENTER);
+		JPanel editNamePanel = new JPanel();
+		editNamePanel.setLayout(new BorderLayout());
+		editNamePanel.setBorder( BorderFactory.createEmptyBorder(10, 0, 0, 0));
+		JLabel editNameLabel = new JLabel("Drink Name:");
+		JTextField infoNameTextField = new JTextField(20);
+		editNamePanel.add(editNameLabel, BorderLayout.WEST);
+		editNamePanel.add(infoNameTextField, BorderLayout.CENTER);
+		informationPanel.add(editNamePanel, BorderLayout.NORTH);
 		
-		JTextArea infoDescriptionTextArea = new JTextArea("DESCRIPTION");
+		
+		JPanel editIngredientPanel = new JPanel();
+		editIngredientPanel.setLayout(new BoxLayout(editIngredientPanel, BoxLayout.PAGE_AXIS));
+		editIngredientPanel.setBorder( BorderFactory.createEmptyBorder(10, 0, 0, 10));
+		informationPanel.add(editIngredientPanel, BorderLayout.CENTER);
+		
+		
+		/*
+		 * Header text for the Drink creation field
+		 */
+		JPanel ingredientPanelArray[] = new JPanel[13];
+		
+		JLabel nameHeaderLabel = new JLabel("Ingredient");
+		nameHeaderLabel.setPreferredSize(new Dimension(150,20));
+		JLabel sizeHeaderLabel = new JLabel("Size (mL)");
+		JPanel sizeHeaderPanel = new JPanel();
+		sizeHeaderPanel.add(sizeHeaderLabel);
+		
+		ingredientPanelArray[12] = new JPanel(new BorderLayout());
+		ingredientPanelArray[12].add(nameHeaderLabel, BorderLayout.WEST);
+		ingredientPanelArray[12].add(sizeHeaderPanel, BorderLayout.CENTER);
+		editIngredientPanel.add(ingredientPanelArray[12]);
+		
+		
+		/*
+		 * Handles the Labels, textfields, and remove buttons
+		 * in the Drink creation field
+		 */
+		JPanel ingredientNamePanel[] = new JPanel[12];
+		JTextField ingredientNameArray[] = new JTextField[12];
+		JPanel textPanelArray[] = new JPanel[12];
+		JTextField amountTextArray[] = new JTextField[12];
+		JPanel removePanelArray[] = new JPanel[12];
+		JButton removeIngredientArray[] = new JButton[12];
+		
+		for (int i = 0; i < 12; i++){
+			
+			ingredientPanelArray[i] = new JPanel(new BorderLayout());
+			ingredientNamePanel[i] = new JPanel();
+			ingredientNameArray[i] = new JTextField(20);
+			ingredientNameArray[i].setEditable(false);
+			ingredientNamePanel[i].add(ingredientNameArray[i]);
+			
+			amountTextArray[i] = new JTextField(3);
+			textPanelArray[i] = new JPanel();
+			textPanelArray[i].add(amountTextArray[i]);
+			
+			
+			removeIngredientArray[i] = new JButton("Remove");
+			removePanelArray[i] = new JPanel();
+			removePanelArray[i].add(removeIngredientArray[i]);
+			
+	
+			ingredientPanelArray[i].add(ingredientNamePanel[i],BorderLayout.WEST);
+			ingredientPanelArray[i].add(textPanelArray[i],BorderLayout.CENTER);
+			ingredientPanelArray[i].add(removePanelArray[i],BorderLayout.EAST);
+			editIngredientPanel.add(ingredientPanelArray[i]);
+		}
+		
+		
+		JPanel descriptionPanel = new JPanel(new BorderLayout());
+		JLabel descriptionLabel = new JLabel("Drink Description:");
+		JTextArea infoDescriptionTextArea = new JTextArea();
 		infoDescriptionTextArea.setRows(5);
-		informationPanel.add(infoDescriptionTextArea, BorderLayout.SOUTH);
+		infoDescriptionTextArea.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		descriptionPanel.add(descriptionLabel, BorderLayout.NORTH);
+		descriptionPanel.add(infoDescriptionTextArea, BorderLayout.CENTER);
+		informationPanel.add(descriptionPanel, BorderLayout.SOUTH);
 		
 		/*
 		 * Menu Bar
@@ -117,9 +350,9 @@ public class ClientGui extends JFrame implements ActionListener{
 		selectDrink.addActionListener(this);
 		edit.add(selectDrink);
 		
-		addDrink = new JMenuItem("Add Drink");
-		addDrink.addActionListener(this);
-		edit.add(addDrink);
+		createDrink = new JMenuItem("Create Drink");
+		createDrink.addActionListener(this);
+		edit.add(createDrink);
 		
 		
 		
@@ -155,7 +388,7 @@ public class ClientGui extends JFrame implements ActionListener{
 		
 		viewer = new JMenu("Map Viewer");
 		find = new JMenuItem("Find");
-		find.addActionListener(this);
+		find.addActionListener(this); 
 		directions = new JMenuItem("Directions");
 		directions.addActionListener(this);
 		
@@ -167,6 +400,8 @@ public class ClientGui extends JFrame implements ActionListener{
 		menuBar.add(viewer);
 		setJMenuBar(menuBar);
 		*/
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		//JFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
 	public static void main(String[] args) {
@@ -182,7 +417,7 @@ public class ClientGui extends JFrame implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		if(e.getSource() == addDrink){
+		if(e.getSource() == createDrink){
 			selectionPanel.setVisible(false);
 			editPanel.setVisible(true);
 			barbotPanel.remove(selectionPanel);
